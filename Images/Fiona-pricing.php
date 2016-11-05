1c209073a50c434a856daa3d7c85b8f1b253e74d
David Martin <davidmartin77@github.com>
2016-11-05 12:21:23 -0500

    adds Fiona-pricing page w header and top container

---
+++ Images/Fiona-pricing.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+	$thisPage="Fiona-Pricing";
+	?>
+
+<head>
+<title>Fiona-Pricing</title>
+
+	<link rel="stylesheet" type="text/css" href="fiona-style.css">
+
+	<link rel="stylesheet" type="text/css" href="./css/font-awesome.css">
+
+</head>
+<body>	
+
+<?php 
+	include 'header.php';
+	?>	
+<!-- 1 container holding three separate containers -->
+<div class = "dealsContainer">
+	
+	<div class = "deals">
+		<h1 class = "deals__headline">Pre &amp; post natal Massages </h1>
+
+		<h1 class = "deals__price">$300</h1>
+		<h1 class = "deals__disclaimer">4 sessions</h1>
+		<p class = "deals__description">Our pre and post natal massages have proven to be effective and absolutely safe, both for woman and their unborn babies...</p>
+		<a href="xxxx" class = "deals__book navigation__link--book">
+		 >Book an appointment</a>
+	</div>	 
+
+	<div class = "deals">
+		<h1 class = "deals__headline">Intensive Massage Sessions</h1>
+
+		<h1 class = "deals__price">$400</h1>
+		<h1 class = "deals__disclaimer">4 sessions</h1>
+		<p class = "deals__description">This massage category is our most popular one and for good reason. It gives you a completely refreshing experience.</p>
+		<a href="xxxx" class = "deals__book navigation__link--book">
+		 >Book an appointment</a>
+	</div>	 
+
+	<div class = "deals">
+		<h1 class = "deals__headline">Extravagent and fun-spirited</h1>
+
+		<h1 class = "deals__price">$500</h1>
+		<h1 class = "deals__disclaimer">4 sessions</h1>
+		<p class = "deals__description">Try out our "Hot Stones" or "Stones and Sticks" massages, which are both fun and good for your overall health and wellbeing!</p>
+		<a href="xxxx" class = "deals__book navigation__link--book">
+		 >Book an appointment</a>
+	</div>	
+
+	<div class = 
+
+<!-- separate container holding contact info
+followed by a container holding happy customer quote
+container holding massage service holding three separate containers -->
+
+
+
+
